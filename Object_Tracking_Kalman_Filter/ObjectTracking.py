0a73b32c63f254ca908b753f2bc3e5abef874dd6
luisivey05 <livey@bu.edu>
2022-08-22 13:35:06 -0700

    [ADDED] YOLOV5 w/ CV2 Annotations

--- Object_Tracking_Kalman_Filter/ObjectTracking.py
+++ Object_Tracking_Kalman_Filter/ObjectTracking.py
@@ -1,31 +1,19 @@
-import sys
 import os
-from google.colab import files
-import centernet
-
-sys.path.append("/content/ObjectTracking")
+#from util import bounding
 from sort import Sort
-from lib import VisTrack, show_video, create_video
-
 import numpy as np
-import PIL.Image
+import torch
 import cv2
-import matplotlib.pyplot as plt
-from tqdm.auto import tqdm
 import shutil
 
 
-video_file = "./video.mp4"
-# Default: num_classes=80
-obj = centernet.ObjectDetection(num_classes=80)
-
-# num_classes=80 and weights_path=None: Pre-trained COCO model will be loaded.
-obj.load_weights(weights_path=None)
-
-vidcap = cv2.VideoCapture(video_file)
+VIDEO_FILE = "./video.mp4"
+DETECT = "person"
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+model.eval()
+vidcap = cv2.VideoCapture(VIDEO_FILE)
 fps = vidcap.get(cv2.CAP_PROP_FPS)
 length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-
 folder_out = "Track"
 if os.path.exists(folder_out):
     shutil.rmtree(folder_out)
@@ -35,34 +23,35 @@ draw_imgs = []
 
 sort = Sort(max_age=1, min_hits=3, iou_threshold=0.3)
 
-pbar = tqdm(total=length)
 i = 0
 while True:
     ret, frame = vidcap.read()
     if not ret:
         break
 
-    boxes, classes, scores = obj.predict(frame)
-    detections_in_frame = len(boxes)
-    if detections_in_frame:
+    results = model(frame).pandas().xyxy
+    scores = results[0]['confidence'].to_numpy(dtype=np.float32)
+    detections = results[0][['xmin', 'ymin', 'xmax', 'ymax', 'confidence']].to_numpy(dtype=np.float32)
+    classes = results[0]['class'].to_numpy()
+
+    if len(detections):
         # centernet will do detection on all the COCO classes. "person" is class number 0
         idxs = np.where(classes == 0)[0]
-        boxes = boxes[idxs]
+        detections = detections[idxs]
         scores = scores[idxs]
         classes = classes[idxs]
     else:
         boxes = np.empty((0, 5))
 
-    dets = np.hstack((boxes, scores[:,np.newaxis]))
-    res = sort.update(dets)
+    res = sort.update(detections)
 
-    boxes_track = res[:,:-1]
-    boces_ids = res[:,-1].astype(int)
+    boxes_track = res[:,:-1].astype(int)
+    boxes_ids = res[:,-1].astype(int)
 
-    p_frame = PIL.Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    if detections_in_frame:
-        p_frame = vt.draw_bounding_boxes(p_frame, boxes_track, boces_ids, scores)
-    p_frame.save(os.path.join(folder_out, f"{i:03d}.png"))
+    for bbox, id, score in zip(boxes_track, boxes_ids, scores):
+        text = "{} ID:{} Score:{:.2f}".format(DETECT, id, score)
+        cv2.putText(frame, text, (bbox[0], bbox[1] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 1)  # Annotate the frame to be sent back to the main driver
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 1)
 
-    i+=1
-    pbar.update(1)
+    i += 1
+    cv2.imwrite("./track/frame{}.png".format(i), frame)
