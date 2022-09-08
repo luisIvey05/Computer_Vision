9f9b8cdca02b6f5df896bf059ebc21a9f874c24d
luisivey05 <livey@bu.edu>
2022-09-07 22:06:36 -0400

    [PATCH] FIXED OUTPUT VIDEO

--- Real_Time_HydraNet/main.py
+++ Real_Time_HydraNet/main.py
@@ -90,19 +90,19 @@ def main(source, fpath):
         video = cv2.VideoCapture(fpath)
         if video.isOpened() == False:
             print("[INFO] ERROR OPENING VIDEO STREAM OR FILE")
-        video = []
+        output_video = []
         while video.isOpened():
             (check, frame) = video.read()
             if check:
                 h, w, _= frame.shape
                 frame = np.array(frame)
                 depth, segm = pipeline(frame, hydranet, NUM_CLASSES, CMAP)
-                video.append(cv2.cvtColor(cv2.vconcat([frame, segm, depth]), cv2.COLOR_BGR2RGB))
+                output_video.append(cv2.cvtColor(cv2.vconcat([frame, segm, depth]), cv2.COLOR_BGR2RGB))
             else:
                 break
         out = cv2.VideoWriter('./out.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 15, (w, 3*h))
-        for i in range(len(video)):
-            out.write(video[i])
+        for i in range(len(output_video)):
+            out.write(output_video[i])
         video.release()
 
     cv2.destroyAllWindows()
